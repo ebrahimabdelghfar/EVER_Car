4f50e9c474f3d897a056c48c96d5e6cd60f6aeb6
ebrahimabdelghfar <ebrahimabdelghfar550@gmail.com>
2024-05-11 13:39:06 +0300

    [FIX BUG]  Changes to be committed: 	modified:   car_api/car_api/MatgrCar.py

--- car_api/car_api/MatgrCar.py
+++ car_api/car_api/MatgrCar.py
@@ -6,11 +6,11 @@ import struct
 
 class MatgrCar(Node):
     def __init__(self):
-        super().__init__('MatgrCar')
+        # super().__init__('MatgrCar')
         #connect to the car
         self.ser = serial.Serial('/dev/ttyUSB0', 5000000)
-        self.publisher_ = self.create_subscription(AckermannDrive, '/matgrCarController',self.moveCar, 10)
-        self.create_timer(0.1, self.readSteeringAngle)
+        # self.publisher_ = self.create_subscription(AckermannDrive, '/matgrCarController',self.moveCar, 10)
+        # self.create_timer(0.1, self.readSteeringAngle)
 
     def collectSteeringAngle(self) -> float:
         """
@@ -57,16 +57,15 @@ class MatgrCar(Node):
         self.ser.write(bytes_data) # Send bytes over serial
 
     def readSteeringAngle(self):
-        steering_angle = self.car.collectSteeringAngle()
+        steering_angle = self.collectSteeringAngle()
         self.get_logger().info('Steering angle: "%s"' % steering_angle)
 
     def moveCar(self, msg: AckermannDrive):
         self.get_logger().info('Publishing: "%s"' % msg)
         #m/sec to km/h
         msg.speed = msg.speed * 3.6
-        self.car.sendCommandTocar(steering=msg.steering_angle, velocity=msg.speed)
-
-        
+        self.sendCommandTocar(steering=msg.steering_angle, velocity=msg.speed)
+     
 def main(args=None):
     rclpy.init(args=args)
     matgr = MatgrCar()
