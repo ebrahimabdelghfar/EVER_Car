c1349f9648af095bc8f0c1d2193a7bf475d02884
ebrahimabdelghfar <ebrahimabdelghfar550@gmail.com>
2024-05-11 23:52:36 +0300

    [FIX BUG]

--- car_api/car_api/MatgrCar.py
+++ car_api/car_api/MatgrCar.py
@@ -14,8 +14,6 @@ class MatgrCar(Node):
                                                    topic = '/matgrCarController',
                                                    callback = self.moveCar,
                                                    qos_profile = 10)
-        
-        self.create_timer(timer_period_sec = 0.1, callback = self.readSteeringAngle)
     
     def sendCommandTocar(self,steering:float = 0.0 , velocity:float = 0.0):
         """
@@ -46,13 +44,11 @@ class MatgrCar(Node):
         bytes_data = struct.pack('<' + 'f'*num_elements, *vector_data) # Convert vector to bytes
         self.ser.write(bytes_data) # Send bytes over serial
 
-    def readSteeringAngle(self):
-        steering_angle = self.collectSteeringAngle()
-        self.get_logger().info('Steering angle: "%s"' % steering_angle)
-
     def moveCar(self, msg: AckermannDrive):
         #m/sec to km/h
         msg.speed = msg.speed * 3.6
+        #rad to degree
+        msg.steering_angle = msg.steering_angle * 57.2958
         self.sendCommandTocar(steering=msg.steering_angle, velocity=msg.speed)
      
 def main(args=None):
